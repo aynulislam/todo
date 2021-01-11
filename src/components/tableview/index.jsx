8a47dad2dc1b70ce9503c5c110f1cf1381df3fed
aynulislam <itamxeed7@gmail.com>
2021-01-11 17:44:05 +0600

    react todo  project practice part2 done

---
+++ src/components/tableview/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Table,CustomInput,Button} from 'reactstrap';
+
+const RowItem = (todo,toggleSelect,toggleComplete) => {
+    return (
+    <tr>
+        <th scope="row">
+            <CustomInput
+            type = 'checkbox'
+            id = {todo.id}
+            checked={todo.isSelect}
+            onChange={() => toggleSelect(todo.id)}
+            />
+        </th>
+        <th>{todo.time}</th>
+        <th>{todo.text}</th>
+
+        <th>
+            <Button
+                color={todo.isComplete? 'danger':'success'}
+                onClick={() => toggleComplete(todo.id)}
+            >
+                {todo.isComplete? 'Completed' : 'Running'}
+            </Button>
+        </th>
+    </tr>
+    );
+};
+
+RowItem.prototype={
+    todo: PropTypes.object.isRequired,
+    toggleSelect: PropTypes.func.isRequired,
+    toggleComplete: PropTypes.func.isRequired,
+};
+
+
+const TableView = ({todos, toggleSelect, toggleComplete}) => (
+    <Table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Time</th>
+                <th>Todo</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {todos.map(todo => (
+                <RowItem
+                key={todo.id}
+                todo={todo}
+                toggleSelect={toggleSelect}
+                toggleComplete={toggleComplete}
+                />
+            
+            ))}
+        </tbody>
+
+    </Table>
+);
+
+TableView.prototype={
+    todos: PropTypes.object.isRequired,
+    toggleSelect: PropTypes.func.isRequired,
+    toggleComplete: PropTypes.func.isRequired
+};
+
+export default TableView;
--- src/components/todos/index.jsx
+++ src/components/todos/index.jsx
@@ -1,17 +1,72 @@
 import React from 'react';
+import ListView from '../listview';
+import TableView from '../tableview';
+
+
+
 
 
 class Todos extends React.Component{
+
+    state = {
+        todos: [
+            {
+                id: '1',
+                text: 'my first text',
+                description: 'this is first text description',
+                time: new Date(),
+                isComplete: false,
+                isSelect: false
+            },
+            {
+                id: '2',
+                text: 'my second text',
+                description: 'this is second text description',
+                time: new Date(),
+                isComplete: true,
+                isSelect: false
+            }            
+        ]
+    };    
+
+    toggleSelect = todoId => {
+
+    };
+
+    toggleComplete = todoId => {
+
+    };
 
     render(){
         return(
         <div>
             <h1 className="display-4 text-center mb-5"><strong>My Todo App</strong></h1>
+        
+            <div>
+                <ListView
+                todos={this.state.todos}
+                toggleSelect={this.state.toggleSelect}
+                toggleComplete={this.state.toggleComplete}
+                
+                />
+            </div>
+
+            <div>
+                <TableView
+                    todos={this.state.todos}
+                    toggleSelect={this.state.toggleSelect}
+                    toggleComplete={this.state.toggleComplete}
+
+                />
+            </div>
+
         </div>
-    
+        
+
         );
 
     }
 }
 
 export default Todos;
+
