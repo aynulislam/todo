a04721e60f54e1e61f3e5c11db602548146e0c90
aynulislam <itamxeed7@gmail.com>
2021-01-11 13:55:39 +0600

    react todo  project initialized

---
+++ src/components/todos/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Todos extends React.Component{
+
+    render(){
+        return(
+        <div>
+            <h1 className="display-4 text-center mb-5"><strong>My Todo App</strong></h1>
+        </div>
+    
+        );
+
+    }
+}
+
+export default Todos;
